d0c5f4552f8e203b19aecf19a99586d914832b21
Czulkuaga <ce13sar12@gmail.com>
2025-10-22 11:55:31 -0500

    cesar.zuluaga add build data

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: true,
+  experimental: {
+    serverSourceMaps: true,
+  },
   images: {
     remotePatterns: [
       {
